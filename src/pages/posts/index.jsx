9304f59d540944b18a6b6a0fcb4c052ea3822841
mrdotb <mrdotb@protonmail.com>
2023-04-09 21:42:33 +0200

    posts index update text

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -38,15 +38,15 @@ export default function PostsIndex({ posts }) {
   return (
     <>
       <Head>
-        <title>Posts - Spencer Sharp</title>
+        <title>Posts - Baptiste Chaleil</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="My notes and posts about various topics on software development"
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="My notes and posts about various topics on software development"
+        intro="Here, I share my knowledge, encompassing everything from succinct advice to extensive series, organized chronologically."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
