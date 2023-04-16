cd7f7b2d1fe196a3a89bd310eb3f0d87ea7ee9f3
mrdotb <mrdotb@protonmail.com>
2023-04-16 21:28:43 +0200

    small detail on ArrowDownIcon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -147,7 +147,7 @@ function Resume() {
       </ol>
       <Button href="https://www.linkedin.com/in/baptiste-chaleil-9494a775/" className="group mt-6 w-full">
         Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+        <ArrowDownIcon className="h-4 w-4 stroke-white" />
       </Button>
     </div>
   )
