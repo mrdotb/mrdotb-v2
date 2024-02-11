ec04ea4c45d46f40b830371312c969c949c6dcbc
mrdotb <mrdotb@protonmail.com>
2024-02-11 15:12:03 +0100

    move back to saas plausible

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -21,7 +21,7 @@ export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
 
   return (
-    <PlausibleProvider customDomain="https://plausible.mrdotb.com" domain="mrdotb.com">
+    <PlausibleProvider customDomain="https://a.mrdotb.com" domain="mrdotb.com">
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
